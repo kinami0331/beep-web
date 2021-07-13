a4a9cb9c33a8949e0180e3ad62b04bb6b4802b07
kinami <sunhaonan331@outlook.com>
2021-07-13 16:02:46 +0800

    清理无用代码

--- src/compoments/DataGroupCard.js
+++ src/compoments/DataGroupCard.js
@@ -36,69 +36,6 @@ const DataGroupCard = (props) => {
     console.log(exprInfo)
     console.log(options)
 
-    // const options = [
-    //     {
-    //         label: exprInfo.deviceList[0] + " to " + exprInfo.deviceList[1] + " 原始信号",
-    //         value: exprInfo.deviceList[0] + "_to_" + exprInfo.deviceList[1] + "_fig1.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[0] + " to " + exprInfo.deviceList[1] + " 第一段声音",
-    //         value: exprInfo.deviceList[0] + "_to_" + exprInfo.deviceList[1] + "_fig2.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[0] + " to " + exprInfo.deviceList[1] + " 第二段声音",
-    //         value: exprInfo.deviceList[0] + "_to_" + exprInfo.deviceList[1] + "_fig3.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[0] + " to " + exprInfo.deviceList[1] + " 第一段互相关",
-    //         value: exprInfo.deviceList[0] + "_to_" + exprInfo.deviceList[1] + "_fig4.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[0] + " to " + exprInfo.deviceList[1] + " 第一段峰值选择",
-    //         value: exprInfo.deviceList[0] + "_to_" + exprInfo.deviceList[1] + "_fig5.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[0] + " to " + exprInfo.deviceList[1] + " 第二段互相关",
-    //         value: exprInfo.deviceList[0] + "_to_" + exprInfo.deviceList[1] + "_fig6.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[0] + " to " + exprInfo.deviceList[1] + " 第二段峰值选择",
-    //         value: exprInfo.deviceList[0] + "_to_" + exprInfo.deviceList[1] + "_fig7.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[1] + " to " + exprInfo.deviceList[0] + " 原始信号",
-    //         value: exprInfo.deviceList[1] + "_to_" + exprInfo.deviceList[0] + "_fig1.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[1] + " to " + exprInfo.deviceList[0] + " 第一段声音",
-    //         value: exprInfo.deviceList[1] + "_to_" + exprInfo.deviceList[0] + "_fig2.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[1] + " to " + exprInfo.deviceList[0] + " 第二段声音",
-    //         value: exprInfo.deviceList[1] + "_to_" + exprInfo.deviceList[0] + "_fig3.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[1] + " to " + exprInfo.deviceList[0] + " 第一段互相关",
-    //         value: exprInfo.deviceList[1] + "_to_" + exprInfo.deviceList[0] + "_fig4.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[1] + " to " + exprInfo.deviceList[0] + " 第一段峰值选择",
-    //         value: exprInfo.deviceList[1] + "_to_" + exprInfo.deviceList[0] + "_fig5.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[1] + " to " + exprInfo.deviceList[0] + " 第二段互相关",
-    //         value: exprInfo.deviceList[1] + "_to_" + exprInfo.deviceList[0] + "_fig6.png"
-    //     },
-    //     {
-    //         label: exprInfo.deviceList[1] + " to " + exprInfo.deviceList[0] + " 第二段峰值选择",
-    //         value: exprInfo.deviceList[1] + "_to_" + exprInfo.deviceList[0] + "_fig7.png"
-    //     }
-    // ]
-
-    const defaultOptions = [
-        exprInfo.deviceList[0] + " to " + exprInfo.deviceList[1] + " 第一段互相关"
-    ]
-
     const [selectedImgUrl, setSelectedImgUrl] = useState(null);
 
     let dataCardList = [];
@@ -125,7 +62,6 @@ const DataGroupCard = (props) => {
                     style={{width: '100%'}}
                     placeholder="Please select"
                     options={options}
-                    // defaultValue={defaultOptions}
                     onChange={(value) => {
                         console.log(value);
                         setSelectedImgUrl(value);
